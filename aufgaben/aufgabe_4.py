a10db5a8cfac9411c05032bd3091a48e4280ae44
Eric Winter <ericwinter1996@gmail.com>
2022-09-29 13:59:14 +0200

    solved task 4

--- aufgaben/aufgabe_4.py
+++ aufgaben/aufgabe_4.py
@@ -1,6 +1,16 @@
+# 4a)
+
 def rotate_left_str(s: str, n: int) -> str:
     return s[n:] + s[:n]
 
 
 for i in range(10):
     print(rotate_left_str("Hello World", i))
+
+# 4b)
+
+def move2_str(s: str) -> str:
+    return s[1::2] + s[::2]
+
+
+print(move2_str('abcdefgh'))
